e70abe132d3b42edbcdaebe38495b900a89ea6b0
Gideon Paul <gidzpaul95@gmail.com>
2019-07-08 19:29:52 -0400

    Everything is ready to make a POST API call

--- app/src/main/java/com/example/geniusplazacodingtest/AddUser.java
+++ app/src/main/java/com/example/geniusplazacodingtest/AddUser.java
@@ -3,8 +3,12 @@ package com.example.geniusplazacodingtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.geniusplazacodingtest.R;
+import com.example.geniusplazacodingtest.api.RetrofitClient;
+import com.example.geniusplazacodingtest.api.UserAPI;
 
 public class AddUser extends AppCompatActivity {
 
@@ -12,5 +16,27 @@ public class AddUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+    }
+
+    //Creates user
+    public void createUser(View view) {
+
+        //Extract the text from the view
+        //By deafault, they'll be empty Strings
+        EditText first_name_edit_text = (EditText) findViewById(R.id.first_name);
+        EditText last_name_edit_text = (EditText) findViewById(R.id.last_name);
+        EditText email_edit_text = (EditText) findViewById(R.id.email);
+
+        //Extarct the texts from the view
+        String first_name = first_name_edit_text.getText().toString();
+        String last_name = last_name_edit_text.getText().toString();
+        String email = email_edit_text.getText().toString();
+
+        //Get the api client
+        UserAPI service = RetrofitClient.getRetrofitClient().create(UserAPI.class);
+
+        //TODO: Make a post call
+        
+
     }
 }
