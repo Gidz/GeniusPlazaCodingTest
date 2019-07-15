6a5331df5b621ad3683321b03070598715ba767d
Gideon Paul <gidzpaul95@gmail.com>
2019-07-15 03:54:01 -0400

    Added RetrofitModule

--- app/src/main/java/com/example/geniusplazacodingtest/di/RetrofitModule.java
+++ app/src/main/java/com/example/geniusplazacodingtest/di/RetrofitModule.java
@@ -1,4 +1,24 @@
 package com.example.geniusplazacodingtest.di;
 
+import javax.inject.Singleton;
+
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitModule {
+    @Provides
+    @Singleton
+    public Retrofit provideRetrofit()
+    {
+        Retrofit client;
+        String BASE_URL = "https://reqres.in/api/";
+
+        client = new retrofit2.Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        
+        return client;
+    }
 }
